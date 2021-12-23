54a33ce5080af5b4ae2a016522b66e99e72af114
Dyjo <le5emejo@hotmail.com>
2021-12-23 09:04:05 +0100

    Ajout des exercices en python

--- exercisse3.py
+++ exercisse3.py
@@ -1,7 +1,6 @@
-first_name = str(input("Quel est votre prénom?"))
-first_name_reversed = ""
 
-print("Bonjour "+first_name)
-for letter in first_name :
-    first_name_reversed = letter + first_name_reversed
-print("votre prénom a l'enver est " + first_name_reversed)
+name =str(input("Quel est votre prénom?"))
+
+reversed_name = name[ ::-1]
+
+print(reversed_name)
